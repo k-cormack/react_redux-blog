e71fab195c90515da47fdd663fead706e4f63774
Keith Cormack <kcormack@definitivelogic.com>
2019-03-03 22:04:12 -0700

    lodash/memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
 
 export const fetchPosts = () => {
   return async function(dispatch, getState) {
@@ -15,10 +17,24 @@ export const fetchPosts = () => {
 //     dispactch ({ type: 'FETCH_POSTS', payload: response })
 //  };
 
+// export const fetchUser = (id) => {
+//   return async function(dispatch, getState) {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+//   };
+// };
+
+// WITHOUT lodash/memoize above, WITH lodash/memoize below
+
 export const fetchUser = (id) => {
-  return async function(dispatch, getState) {
+  return function(dispatch, getState) {
+    _fetchUser(id, dispatch);
+  };
+};
+
+  const _fetchUser = _.memoize(async(id, dispatch) => {    // NOTICE the movement of the async parameter
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-  };
-};
+});
